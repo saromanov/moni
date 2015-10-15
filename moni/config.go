5a3ffd9d78d49a24656c549839ec5a37cf5eb191
Sergey Romanov <xxsmotur@gmail.com>
2015-10-15 21:54:18 +0500

    CHange structure

--- moni/config.go
+++ moni/config.go
@@ -1,23 +1,22 @@
 package main
 
-import
-(
-	"time"
+import (
 	"github.com/saromanov/goconfig"
 	"log"
+	"time"
 )
 
 //Config provides basic configuration for moni
 type Config struct {
-	Timeout time.Duration
-	Email   string
+	Timeout  time.Duration
+	Email    string
 	Telegram string
 	Hosts    []*Host
 }
 
 type Host struct {
-	Username  string
-	Password  string
+	Username string
+	Password string
 }
 
 //LoadConfigData provides load configuration or set default params
@@ -27,19 +26,18 @@ func LoadConfigData(path string) *Config {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	conf.setMissedValues()
 	return &conf
 }
 
-
 //TODO
 func (conf *Config) setMissedValues() {
-	
+
 }
 
 func setDefaultParams() *Config {
 	conf := new(Config)
-	conf.Timeout = 60* time.Second
+	conf.Timeout = 60 * time.Second
 	return conf
 }
