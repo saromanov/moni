7a53cd5b3386d0ca2a84c11e4152391a3a6c2a55
Sergey Romanov <xxsmotur@gmail.com>
2015-11-15 23:16:06 +0500

    Check: Update

--- moni/check.go
+++ moni/check.go
@@ -4,4 +4,15 @@ package moni
 // And 1 in otherwise
 type Check struct {
 	Path string
+	Completed int
+	Failed int
+	PercentCompleted float64
+}
+
+func (ch*Check) Complete() {
+	ch.Completed++
+}
+
+func(ch*Check) Fail() {
+	ch.Failed++
 }
